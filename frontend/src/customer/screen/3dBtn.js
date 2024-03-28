5d1c641a4bee331940081d1c083f7119bd77d599
muhammaduzair22 <uzair.bh2000@gmail.com>
2024-03-29 00:23:00 +0500

    v12

--- frontend/src/customer/screen/3dBtn.js
+++ frontend/src/customer/screen/3dBtn.js
@@ -26,7 +26,7 @@ export default function App() {
                 whileTap="press"
                 variants={{
                     rest: { scale: 1 },
-                    hover: { scale: 1.3 },
+                    hover: { scale: 1.2 },
                     press: { scale: 1.4 }
                 }}
                 onHoverStart={() => {
